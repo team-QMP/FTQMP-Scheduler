d7c5f90932f9b895bb06a88f41c4940912b00c8b
Sakumatcho <sakumadaisuke32@gmail.com>
2024-11-29 09:51:22 +0900

    feat #1 create schedule and placement of polycubes

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate kiss3d;
 use qmp_scheduler::visualizer;
 use qmp_scheduler::ds::polycube::Polycube;
 use qmp_scheduler::ds::polycube::Coordinate;
+use qmp_scheduler::ds::schedule::Schedule;
 
 fn create_basis_polyblock() -> Polycube{
     let mut poly_cube = Polycube::new(vec![Coordinate::new(0, 0, 0)]);
@@ -23,7 +24,7 @@ fn create_basis_polyblock() -> Polycube{
     return poly_cube;
 }
 
-fn create_test_polyblock() -> Polycube{
+fn create_test_polyblock() -> (Polycube, Vec<Schedule>) {
     let mut poly_cube = Polycube::new(vec![Coordinate::new(0, 0, 0)]);
     let pos_candidate_list: Vec<Coordinate> = vec![
         Coordinate{x: 0, y: 1, z: 0},
@@ -42,13 +43,16 @@ fn create_test_polyblock() -> Polycube{
         Coordinate{x: 0, y: 6, z: 3},
     ];
     // 移動量、回転量、反転を定義する
+    let cube_settings: Vec<Schedule> = vec![
+        Schedule{x: 3, y: 2, z: 1, rotate: 0, flip: false},
+    ];
     for i in 0..pos_candidate_list.len(){
         let pos = &pos_candidate_list[i];
         println!("{:?}", pos);
         poly_cube.add_block(pos.clone());
     }
     println!("{:?}", poly_cube);
-    return poly_cube;
+    return (poly_cube, cube_settings);
 }
 
 fn main() {
@@ -58,13 +62,13 @@ fn main() {
     // ランダムだとテトリスできなくて困るので、固定でポリキューブのセットを作る
     // 原点もよくわからないので、原点用のポリキューブをまず作る。
     let basis_polycube = create_basis_polyblock();
-    let test_polycube = create_test_polyblock();
-    let polycubes: Vec<Polycube> = vec![basis_polycube, test_polycube];
-    visualizer::render_cubes(&polycubes);
-
     // 任意の形状のポリキューブを作る
-
     // ポリキューブを回転したり移動させる
+    let (test_polycube, test_cube_settings) = create_test_polyblock();
+    let polycubes: Vec<Polycube> = vec![basis_polycube, test_polycube];
+    visualizer::render_cubes(&polycubes, &test_cube_settings);
+
+
 
     // 任意の形状のポリキューブを複数作る
 
--- src/visualizer.rs
+++ src/visualizer.rs
@@ -5,6 +5,7 @@ use kiss3d::window::Window;
 use kiss3d::light::Light;
 
 use crate::ds::polycube::Polycube;
+use crate::ds::schedule::Schedule;
 
 #[allow(dead_code)]
 pub fn visualize(polycube: &Polycube) {
@@ -24,7 +25,7 @@ pub fn visualize(polycube: &Polycube) {
 
 
 #[allow(dead_code)]
-pub fn render_cubes(polycubes: &Vec<Polycube>) {
+pub fn render_cubes(polycubes: &Vec<Polycube>, cube_settings: &Vec<Schedule>) {
     println!("render cubes");
     let mut window = Window::new("Block Visualize");
     let scale: f32 = 0.1;
@@ -40,9 +41,10 @@ pub fn render_cubes(polycubes: &Vec<Polycube>) {
 
     for i in 1..polycubes.len() {
         let polycube = &polycubes[i];
+        let schedule = &cube_settings[i-1];
         for pos in polycube.blocks() {
             let mut c = window.add_cube(scale*(1.-margin),scale*(1.-margin),scale*(1.-margin));
-            c.append_translation(&Translation3::new((pos.x as f32)*scale+0.3, (pos.y as f32)*scale,(pos.z as f32)*scale));
+            c.append_translation(&Translation3::new((pos.x as f32)*scale+(schedule.x as f32)*scale, (pos.y as f32)*scale+(schedule.y as f32)*scale,(pos.z as f32)*scale+(schedule.z as f32)*scale));
             c.set_color(0., 1., 0., );
         }
     }
