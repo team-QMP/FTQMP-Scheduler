ce03945e37f7dba297e4da8526dfe3f1eb3e5606
Suikaba <suibaka@hotmail.co.jp>
2025-01-10 16:47:21 +0900

    Formatting

--- src/preprocessing.rs
+++ src/preprocessing.rs
@@ -1,7 +1,7 @@
 use kiss3d::ncollide3d::shape::ConvexHull;
-use rand::Rng;
-use qhull::Qh;
 use nalgebra::Vector3;
+use qhull::Qh;
+use rand::Rng;
 use std::f64;
 
 use crate::program::Polycube;
@@ -95,9 +95,15 @@ fn polycube_to_cuboid(polycube: &Polycube) -> Cuboid {
         Some(n) => max_z = *n,
         None => unreachable!(),
     }
-    return Cuboid{min_x: min_x, max_x: max_x, min_y: min_y, max_y: max_y, min_z: min_z, max_z: max_z};
+    return Cuboid {
+        min_x: min_x,
+        max_x: max_x,
+        min_y: min_y,
+        max_y: max_y,
+        min_z: min_z,
+        max_z: max_z,
+    };
 }
-
 
 fn polycube_to_float_coordinates(polycube: &Polycube) -> Vec<FloatCoordinate> {
     // Convert a polycube to a list of points
@@ -106,14 +112,46 @@ fn polycube_to_float_coordinates(polycube: &Polycube) -> Vec<FloatCoordinate> {
     let mut points: Vec<FloatCoordinate> = Vec::new();
     for i in 0..polycube.size() {
         let coordinate = polycube.index_to_xyz(i);
-        points.push(FloatCoordinate{x: coordinate.x as f64, y: coordinate.y as f64, z: coordinate.z as f64}); // add original point
-        points.push(FloatCoordinate{x: coordinate.x as f64 + 1.0, y: coordinate.y as f64, z: coordinate.z as f64}); // add point shifted in x direction
-        points.push(FloatCoordinate{x: coordinate.x as f64, y: coordinate.y as f64 + 1.0, z: coordinate.z as f64}); // add point shifted in y direction
-        points.push(FloatCoordinate{x: coordinate.x as f64, y: coordinate.y as f64, z: coordinate.z as f64 + 1.0}); // add point shifted in z direction
-        points.push(FloatCoordinate{x: coordinate.x as f64 + 1.0, y: coordinate.y as f64 + 1.0, z: coordinate.z as f64}); // add point shifted in x and y direction
-        points.push(FloatCoordinate{x: coordinate.x as f64 + 1.0, y: coordinate.y as f64, z: coordinate.z as f64 + 1.0}); // add point shifted in x and z direction
-        points.push(FloatCoordinate{x: coordinate.x as f64, y: coordinate.y as f64 + 1.0, z: coordinate.z as f64 + 1.0}); // add point shifted in y and z direction
-        points.push(FloatCoordinate{x: coordinate.x as f64 + 1.0, y: coordinate.y as f64 + 1.0, z: coordinate.z as f64 + 1.0}); // add point shifted in x, y and z direction
+        points.push(FloatCoordinate {
+            x: coordinate.x as f64,
+            y: coordinate.y as f64,
+            z: coordinate.z as f64,
+        }); // add original point
+        points.push(FloatCoordinate {
+            x: coordinate.x as f64 + 1.0,
+            y: coordinate.y as f64,
+            z: coordinate.z as f64,
+        }); // add point shifted in x direction
+        points.push(FloatCoordinate {
+            x: coordinate.x as f64,
+            y: coordinate.y as f64 + 1.0,
+            z: coordinate.z as f64,
+        }); // add point shifted in y direction
+        points.push(FloatCoordinate {
+            x: coordinate.x as f64,
+            y: coordinate.y as f64,
+            z: coordinate.z as f64 + 1.0,
+        }); // add point shifted in z direction
+        points.push(FloatCoordinate {
+            x: coordinate.x as f64 + 1.0,
+            y: coordinate.y as f64 + 1.0,
+            z: coordinate.z as f64,
+        }); // add point shifted in x and y direction
+        points.push(FloatCoordinate {
+            x: coordinate.x as f64 + 1.0,
+            y: coordinate.y as f64,
+            z: coordinate.z as f64 + 1.0,
+        }); // add point shifted in x and z direction
+        points.push(FloatCoordinate {
+            x: coordinate.x as f64,
+            y: coordinate.y as f64 + 1.0,
+            z: coordinate.z as f64 + 1.0,
+        }); // add point shifted in y and z direction
+        points.push(FloatCoordinate {
+            x: coordinate.x as f64 + 1.0,
+            y: coordinate.y as f64 + 1.0,
+            z: coordinate.z as f64 + 1.0,
+        }); // add point shifted in x, y and z direction
     }
     return points;
 }
@@ -124,11 +162,14 @@ fn float_coordinates_to_convexhull<'a>(points: &'a Vec<FloatCoordinate>) -> Qh<'
     // output: qh: Qh
     let qh = Qh::builder()
         .compute(true)
-        .build_from_iter(points.iter().map(|p| [p.x, p.y, p.z].to_vec())).unwrap();
+        .build_from_iter(points.iter().map(|p| [p.x, p.y, p.z].to_vec()))
+        .unwrap();
     return qh;
 }
 
-fn convex_hull_to_minimal_enclosing_box(points: &[Vector3<f64>]) -> (Vector3<f64>, Vector3<f64>, f64) {
+fn convex_hull_to_minimal_enclosing_box(
+    points: &[Vector3<f64>],
+) -> (Vector3<f64>, Vector3<f64>, f64) {
     let qh = Qh::builder()
         .compute(true)
         .build_from_iter(points.iter().map(|p| vec![p.x, p.y, p.z]))
@@ -142,13 +183,15 @@ fn convex_hull_to_minimal_enclosing_box(points: &[Vector3<f64>]) -> (Vector3<f64
         if face1.is_sentinel() {
             continue;
         }
-        let normal1 = Vector3::new(face1.normal()[0], face1.normal()[1], face1.normal()[2]).normalize();
+        let normal1 =
+            Vector3::new(face1.normal()[0], face1.normal()[1], face1.normal()[2]).normalize();
 
         for face2 in qh.all_faces() {
             if face2.is_sentinel() || (&face1 as *const _) == (&face2 as *const _) {
                 continue;
             }
-            let normal2 = Vector3::new(face2.normal()[0], face2.normal()[1], face2.normal()[2]).normalize();
+            let normal2 =
+                Vector3::new(face2.normal()[0], face2.normal()[1], face2.normal()[2]).normalize();
 
             if normal1.cross(&normal2).norm() < 1e-6 {
                 // ベクトルが独立していない場合はスキップ
@@ -158,10 +201,7 @@ fn convex_hull_to_minimal_enclosing_box(points: &[Vector3<f64>]) -> (Vector3<f64
             let normal3 = normal1.cross(&normal2).normalize();
             let rotation = nalgebra::Rotation3::from_basis_unchecked(&[normal1, normal2, normal3]);
 
-            let rotated_points: Vec<Vector3<f64>> = points
-                .iter()
-                .map(|p| rotation * p)
-                .collect();
+            let rotated_points: Vec<Vector3<f64>> = points.iter().map(|p| rotation * p).collect();
 
             let (min_point, max_point) = rotated_points.iter().fold(
                 (Vector3::repeat(f64::MAX), Vector3::repeat(f64::MIN)),
@@ -239,27 +279,26 @@ fn convex_hull_to_minimal_enclosing_box(points: &[Vector3<f64>]) -> (Vector3<f64
 //     (best_box_center, best_box_dimensions, min_volume)
 // }
 
-
 #[cfg(test)]
 pub mod test {
     // create random polycube
-    use nalgebra::Vector3;
     use crate::ds::polycube::create_random_polycube;
-    use crate::preprocessing::FloatCoordinate;
-    use crate::preprocessing::Cuboid;
+    use crate::preprocessing::convex_hull_to_minimal_enclosing_box;
     use crate::preprocessing::create_random_floatcoordinates;
+    use crate::preprocessing::float_coordinates_to_convexhull;
     use crate::preprocessing::polycube_to_cuboid;
     use crate::preprocessing::polycube_to_float_coordinates;
-    use crate::preprocessing::float_coordinates_to_convexhull;
     use crate::preprocessing::print_floatcoordinates;
-    use crate::preprocessing::convex_hull_to_minimal_enclosing_box;
+    use crate::preprocessing::Cuboid;
+    use crate::preprocessing::FloatCoordinate;
+    use nalgebra::Vector3;
 
-    # [test]
+    #[test]
     fn test_create_random_floatcoordinates() {
         let points = create_random_floatcoordinates(10); // create random points
         println!("Random points:");
         print_floatcoordinates(&points); // print points
-        // check that the number of points is equal to the input
+                                         // check that the number of points is equal to the input
         assert_eq!(points.len(), 10);
     }
 
@@ -272,7 +311,10 @@ pub mod test {
 
         let cuboid = polycube_to_cuboid(&randompolycube); // convert polycube to Cuboid
         println!("Cuboid:");
-        println!("({}, {}, {}, {}, {}, {})", cuboid.min_x, cuboid.max_x, cuboid.min_y, cuboid.max_y, cuboid.min_z, cuboid.max_z);
+        println!(
+            "({}, {}, {}, {}, {}, {})",
+            cuboid.min_x, cuboid.max_x, cuboid.min_y, cuboid.max_y, cuboid.min_z, cuboid.max_z
+        );
         // check that the cuboid contains all the points in the polycube
         for i in 0..randompolycube.size() {
             let coordinate = randompolycube.index_to_xyz(i);
@@ -292,31 +334,41 @@ pub mod test {
         let points = polycube_to_float_coordinates(&randompolycube); // convert polycube to FloatCoordinates
         println!("Float coordinates:");
         print_floatcoordinates(&points); // print points
-        // check that the number of points is equal to the number of blocks in the polycube
+                                         // check that the number of points is equal to the number of blocks in the polycube
         assert_eq!(points.len(), (randompolycube.size() as usize) * 8);
     }
 
-    # [test]
+    #[test]
     fn test_floatcoordinates_to_convexhull() {
         // create a random float coordinates
         let points = create_random_floatcoordinates(10);
         println!("Random points:");
         print_floatcoordinates(&points);
         // convert float coordinates to convex hull
-        let float_points: Vec<FloatCoordinate> = points.iter().map(|p| FloatCoordinate { x: p.x, y: p.y, z: p.z }).collect();
+        let float_points: Vec<FloatCoordinate> = points
+            .iter()
+            .map(|p| FloatCoordinate {
+                x: p.x,
+                y: p.y,
+                z: p.z,
+            })
+            .collect();
         let convexhull = float_coordinates_to_convexhull(&float_points);
         for (i, face) in convexhull.all_faces().enumerate() {
             let vertices = face.vertices();
             if !face.is_sentinel() {
-                println!("\nFace {}:",i);
-                for vertex in vertices{
+                println!("\nFace {}:", i);
+                for vertex in vertices {
                     // println!("\nvertex:");
                     // println!("{:?}", vertex);
                     // make a list of coordinates of vertex
-                    let coordinates: Vec<_> = vertex.iter().map(|v| {
-                        let point = &points[v.id() as usize];
-                        [point.x, point.y, point.z]
-                    }).collect();
+                    let coordinates: Vec<_> = vertex
+                        .iter()
+                        .map(|v| {
+                            let point = &points[v.id() as usize];
+                            [point.x, point.y, point.z]
+                        })
+                        .collect();
                     println!("{:?}", coordinates);
                 }
             }
@@ -324,19 +376,24 @@ pub mod test {
         assert_eq!(10, 10);
     }
 
-    # [test]
-    fn test_convexhull_to_minimal_enclosing_box(){
+    #[test]
+    fn test_convexhull_to_minimal_enclosing_box() {
         let points = create_random_floatcoordinates(10);
         println!("Random points:");
         print_floatcoordinates(&points);
         // convert float coordinates to convex hull
-        let float_points: Vec<FloatCoordinate> = points.iter().map(|p| FloatCoordinate { x: p.x, y: p.y, z: p.z }).collect();
-        let convexhull = float_coordinates_to_convexhull(&float_points);
-    // FloatCoordinateをVector3<f64>に変換
-        let vector_points: Vec<Vector3<f64>> = points
+        let float_points: Vec<FloatCoordinate> = points
             .iter()
-            .map(|p| Vector3::new(p.x, p.y, p.z))
+            .map(|p| FloatCoordinate {
+                x: p.x,
+                y: p.y,
+                z: p.z,
+            })
             .collect();
+        let convexhull = float_coordinates_to_convexhull(&float_points);
+        // FloatCoordinateをVector3<f64>に変換
+        let vector_points: Vec<Vector3<f64>> =
+            points.iter().map(|p| Vector3::new(p.x, p.y, p.z)).collect();
         // 最小内包ボックスを計算
         let (center, dimensions, volume) = convex_hull_to_minimal_enclosing_box(&vector_points);
 
@@ -344,5 +401,4 @@ pub mod test {
         println!("Box dimensions (W, H, D): {:?}", dimensions);
         println!("Box volume: {:?}", volume);
     }
-
 }
