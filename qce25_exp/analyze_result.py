649479f63b7c6e3d74995345575d79a8ec135003
Suikaba <suibaka@gmail.com>
2025-04-06 22:30:10 +0900

    Response time analysis

--- qce25_exp/analyze_result.py
+++ qce25_exp/analyze_result.py
@@ -13,22 +13,36 @@ def analyze_exp_result(input_folder):
     throughputs = []
     sum_th = 0
     sum_sq_th = 0
+    resp_times = []
     for json_file in json_files:
         with open(json_file, "r", encoding="utf-8") as f:
             data = json.load(f)
         
         th = data.get("total_cycle", [])
+        resps = data.get("response_time", [])
+
         throughputs.append(th)
         sum_th += th 
         sum_sq_th += th * th
 
+        resp_times += resps
+
     th_mean = sum_th // n
     th_stddev = math.sqrt(sum_sq_th / n - th_mean * th_mean)
+
+    sum_resp_time = sum(resp_times)
+    sum_sq_resp_t = sum(map(lambda t: t * t, resp_times))
+    resp_mean = sum_resp_time / len(resp_times)
+    resp_stddev = math.sqrt(sum_sq_resp_t / len(resp_times) - resp_mean * resp_mean)
 
     result["throughput_mean"] = th_mean
     result["throughput_stddev"] = th_stddev
     result["throughput_min"] = min(throughputs)
     result["throughput_max"] = max(throughputs)
+    result["response_time_mean"] = resp_mean
+    result["response_time_stddev"] = resp_stddev
+    result["response_time_min"] = min(resp_times)
+    result["response_time_max"] = max(resp_times)
 
     output_json_file = os.path.join(input_folder, "all-result.txt")
 
